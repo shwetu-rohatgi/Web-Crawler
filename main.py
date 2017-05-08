42ce787446a4b87e39c837e064cee7bb223d0b25
Shwetanshu Rohatgi <shwetanshu.rohatgi@gmail.com>
2017-05-08 14:40:06 +0530

    second update

--- main.py
+++ main.py
@@ -6,5 +6,18 @@ def create_project_folder(directory):
 		print ('Creating new path '+ directory)
 		os.makedirs(directory)
 
+def create_project_file(project_name,base_url):
+	queue = project_name + 'queue.txt' 
+	crawled = project_name + 'crawled.text'
+	if not os.path.isfile(queue):
+		write_file(queue,base_url)
+	if not os.path.isfile(crawled):
+		write_file(crawled,base_url)
+
+def write_file(path,data):
+	f = open(path,"w")
+	f.write(data)
+	f.close()
+	
 
 create_project_folder('thenewboston')
